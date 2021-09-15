0585f9c22eed16c5fffbe212b612805b3f3b6497
brusN <mainbrusmail@gmail.com>
2021-09-15 23:45:26 +0700

    Rewrited

--- labs-2/lab2/lab2.c
+++ labs-2/lab2/lab2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+
 
 #define SUCCESS 0
 #define FAILURE 1
@@ -11,23 +13,29 @@ typedef struct st_argPrintText {
 } argPrintText;
 
 void *printText(void *inputArg) {
-    argPrintText *arg = inputArg;
+    argPrintText *arg = (argPrintText *)inputArg;
     for (int i = 0; i < arg->count; ++i) {
-        printf("%s\n", arg->text);
+        printf("%s", arg->text);
     }
+    return NULL;
+}
+
+void posixError(pthread_t threadID, int code, char *msg) {
+    fprintf(stderr, "[Error in thread id: %lu] %s: %s\n", threadID, msg, strerror(code));
+    exit(EXIT_FAILURE);
 }
 
 int main() {
     pthread_t threadID;
-    argPrintText arg1 = { "Main thread", 10};
-    argPrintText arg2 = { "Another thread", 10};
+    static argPrintText arg1 = {"Main thread\n", 10};
+    static argPrintText arg2 = {"Another thread\n", 10};
 
-    int err = pthread_create(&threadID, NULL, printText, &arg2);
-    if (err != SUCCESS) {
-        fprintf(stderr, "Error while creating thread! Code %d", err);
-        exit(EXIT_FAILURE);
+    int retCode = pthread_create(&threadID, NULL, printText, (void *)&arg2);
+    if (retCode != SUCCESS) {
+        posixError(pthread_self(), retCode, "Error while creating new thread");
     }
 
+    printText((void *)&arg1);
     pthread_join(threadID, NULL);
-    printText(&arg1);
+    return EXIT_SUCCESS;
 }
